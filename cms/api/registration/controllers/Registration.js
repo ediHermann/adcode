0df7ad3eb8acca6cc42a82190d712bbd766313c3
EDI HERMANN <edi.hermann@lemonbyte.ro>
2020-06-05 11:53:15 +0300

    registration +email

--- cms/api/registration/controllers/Registration.js
+++ cms/api/registration/controllers/Registration.js
@@ -1,41 +1,44 @@
 'use strict';
-const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+const { parseMultipartData, sanitizenewRecord } = require('strapi-utils');
 
 //POST
 module.exports = {
   async create(ctx) {
-    let entity;
+    let newRecord;
+
+
+
     if (ctx.is('multipart')) {
       const { data, files } = parseMultipartData(ctx);
-      entity = await strapi.services.user.create(data, {files });
+      newRecord = await strapi.services.user.create(data, {files });
     } else {
 
       try {
 
-
         //insert user
-        entity = await strapi.plugins['users-permissions'].services.user.add(ctx.request.body);
-        console.log(entity.id);
-        const id=entity.id;
+        newRecord = await strapi.plugins['users-permissions'].services.user.add(ctx.request.body);
+        console.log(newRecord.id);
+        const id=newRecord.id;
+        const email=newRecord.email;
+        const username=newRecord.username;
 
         //update to blocked
         const updateData={"blocked":true};
         await strapi.plugins['users-permissions'].services.user.edit({id}, updateData);
 
-        //TODO:
-        // email to Strapi admin
-        // using Twilio SendGrid's v3 Node.js Library
 
+        // email to site admin
 
         const sgMail = require('@sendgrid/mail');
-        sgMail.setApiKey('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');
-
+        const apiKey = strapi.config['sendgrid'].apikey;
+        // sgMail.setApiKey('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');
+        sgMail.setApiKey(apiKey);
         const msg = {
           to: 'edi.hermann@lemonbyte.ro',
           from: 'edi.hermann@lemonbyte.ro',
-          subject: 'Sending with Twilio SendGrid is Fun',
-          text: 'and easy to do anywhere, even with Node.js',
-          html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+          subject: 'Adcode registration request',
+          text:  'A new registration request has arrived. Name:'+ username+', email: '+email,
+          html: '<strong>A new registration request has arrived</strong><br />Name:'+ username+'<br />email: '+email
         };
        //ES6
         sgMail
@@ -47,9 +50,6 @@ module.exports = {
               console.error(error.response.body)
             }
           });
-
-
-
 
 
         // email to user
