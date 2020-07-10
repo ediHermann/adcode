877bf828950de3c32c054ced4f63b17ae01d1d68
EDI HERMANN <edi.hermann@lemonbyte.ro>
2020-07-10 16:22:35 +0300

    UserSpot Paging

--- cms/api/user-spot/config/schema.graphql.js
+++ cms/api/user-spot/config/schema.graphql.js
@@ -3,7 +3,7 @@ const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 module.exports = {
   query: `userSpots(where: JSON, limit: Int, offset: Int, sort:String): JSON!`,
   mutation:`createUserSpot(data: JSON): JSON!,updateUserSpot(uid:String, data: JSON): JSON!,deleteUserSpot(uid:String): JSON! `,
-    resolver: {
+  resolver: {
     Query: {
       userSpots:
         {
@@ -49,11 +49,11 @@ module.exports = {
                   criteria.user = userid;
                   criteria.deleted = false;
                   console.log(criteria)
-                   result = await strapi.services.spot.find(criteria);
-                  }
-                  //console.log(result);
+                  result = await strapi.services.spot.find(criteria);
                 }
-                catch (e) {
+                //console.log(result);
+              }
+              catch (e) {
                 console.log(e);
                 errNum = "202";
                 errDesc = '202 Invalid token: Token did not contain required fields';
@@ -79,234 +79,234 @@ module.exports = {
     },
 
     Mutation: {
-        createUserSpot: {
-            description: 'Create new  spots',
-            resolverOf: 'application::spot.spot.find',
-            resolver: async (obj, options, ctx) => {
-              let newRecord;
-              let userid;
-              let result;
-              let payload;
-              let errNum;
-              let errDesc;
+      createUserSpot: {
+        description: 'Create new  spots',
+        resolverOf: 'application::spot.spot.find',
+        resolver: async (obj, options, ctx) => {
+          let newRecord;
+          let userid;
+          let result;
+          let payload;
+          let errNum;
+          let errDesc;
 
-              //GET USER FROM HEADER TOKEN
-              if (ctx.context.request && ctx.context.request.header && ctx.context.request.header.authorization) {
-                try {
-                  const decrypted = await strapi.plugins['users-permissions'].services.jwt.getToken(ctx.context);
-                  userid = decrypted.id;
-                } catch (e) {
-                  errNum = "201";
-                  errDesc = 'Invalid token: Token did not contain required fields';
-
-                }
-              }
-              if (userid === undefined) {
-                errNum = "201";
-                errDesc = 'Invalid token: Token did not contain required fields';
-              } else {
-                //Authentiation OK
-                try {
-
-                  //insert spot
-                  newRecord = await strapi.services.spot.create(ctx.context.request.body); //Or options.data
-                  console.log(newRecord.id);
-                  const id = newRecord.id;
-
-                  //update the owner
-                  const updateData = {"user": userid};
-                  payload = await strapi.services.spot.update({id}, updateData);
-
-                  result = {
-                    "success": true,
-                    "payload": payload
-                  };
-
-                } catch (error) {
-                  console.log(error);
-                  result = {
-                    "success": false,
-                    "payload": {},
-                    "error": {"code": error.errno, "message": "An error occurred! " + error.code}
-                  };
-
-
-                }
-              }
-              if (newRecord) {
-                //console.log(result);
-                return result;
-
-              } else {
-
-                result = {
-                  "success": false,
-                  "payload": {},
-                  "error": {"code": error.errno, "message": "An error occurred! " + error.code}
-                };
-                return result;
-              }
+          //GET USER FROM HEADER TOKEN
+          if (ctx.context.request && ctx.context.request.header && ctx.context.request.header.authorization) {
+            try {
+              const decrypted = await strapi.plugins['users-permissions'].services.jwt.getToken(ctx.context);
+              userid = decrypted.id;
+            } catch (e) {
+              errNum = "201";
+              errDesc = 'Invalid token: Token did not contain required fields';
 
             }
+          }
+          if (userid === undefined) {
+            errNum = "201";
+            errDesc = 'Invalid token: Token did not contain required fields';
+          } else {
+            //Authentiation OK
+            try {
+
+              //insert spot
+              newRecord = await strapi.services.spot.create(ctx.context.request.body); //Or options.data
+              console.log(newRecord.id);
+              const id = newRecord.id;
+
+              //update the owner
+              const updateData = {"user": userid};
+              payload = await strapi.services.spot.update({id}, updateData);
+
+              result = {
+                "success": true,
+                "payload": payload
+              };
+
+            } catch (error) {
+              console.log(error);
+              result = {
+                "success": false,
+                "payload": {},
+                "error": {"code": error.errno, "message": "An error occurred! " + error.code}
+              };
+
+
+            }
+          }
+          if (newRecord) {
+            //console.log(result);
+            return result;
+
+          } else {
+
+            result = {
+              "success": false,
+              "payload": {},
+              "error": {"code": error.errno, "message": "An error occurred! " + error.code}
+            };
+            return result;
+          }
+
+        }
       },
 
       updateUserSpot: {
-          description: 'Update user  spot',
-          resolverOf: 'application::spot.spot.find',
-          resolver: async (obj, options, ctx) => {
-            const uid = options.uid;
-            let newRecord;
-            let userid;
-            let result;
-            let payload;
-            let errNum;
-            let errDesc;
-            let Success = false;
-           //GET USER FROM HEADER TOKEN
-            if (ctx.context.request && ctx.context.request.header && ctx.context.request.header.authorization) {
-              try {
+        description: 'Update user  spot',
+        resolverOf: 'application::spot.spot.find',
+        resolver: async (obj, options, ctx) => {
+          const uid = options.uid;
+          let newRecord;
+          let userid;
+          let result;
+          let payload;
+          let errNum;
+          let errDesc;
+          let Success = false;
+          //GET USER FROM HEADER TOKEN
+          if (ctx.context.request && ctx.context.request.header && ctx.context.request.header.authorization) {
+            try {
 
 
-                  const decrypted = await strapi.plugins['users-permissions'].services.jwt.getToken(ctx.context);
-                  userid = decrypted.id;
-              } catch (e) {
-                console.log(e)
-                errNum = "201";
-                errDesc = 'Invalid token: Token did not contain required fields';
-
-              }
-            }
-            if (userid === undefined) {
-              errNum = "201";
-              errDesc = 'User undefined';
-            } else {
-              //Authentiation OK
-              try {
-
-
-                //CHECK if the item  belongs to crt user
-                const chkResult = await strapi.services.spot.find({uid: uid, user: userid, deleted: false});
-                 if (chkResult.length > 0) {
-                  const id = chkResult[0].id;
-                  payload = await strapi.services.spot.update({id}, options.data);
-                  Success = true;
-                  result = {
-                    "success": Success,
-                    "payload": payload
-                  };
-
-                } else {
-                  errNum = "301";
-                  errDesc = 'Invalid spot id';
-                }
-
-
-
-              } catch (error) {
-                console.log(error);
-                result = {
-                  "success": false,
-                  "payload": {},
-                  "error": {"code": error.errno, "message": "An error occurred! " + error.code}
-                };
-
-
-              }
-            }
-            if (Success) {
-              //console.log(result);
-              return result;
-
-            } else {
-
-              result = {
-                "success": false,
-                "payload": {},
-                "error": {"code": errNum, "message": errDesc}
-              };
-              return result;
-            }
-          }
-       },
-
-      deleteUserSpot: {
-          description: 'Delete user spot',
-          resolverOf: 'application::spot.spot.find',
-          resolver: async (obj, options, ctx) => {
-            const uid = options.uid;
-            let newRecord;
-            let userid;
-            let result;
-            let payload;
-            let errNum;
-            let errDesc;
-            let Success = false;
-
-
-            //GET USER FROM HEADER TOKEN
-            if (ctx.context.request && ctx.context.request.header && ctx.context.request.header.authorization) {
-              try {
-                const decrypted = await strapi.plugins['users-permissions'].services.jwt.getToken(ctx.context);
-                userid = decrypted.id;
-              } catch (e) {
-                errNum = "201";
-                errDesc = 'Invalid token: Token did not contain required fields';
-
-              }
-            }
-            if (userid === undefined) {
+              const decrypted = await strapi.plugins['users-permissions'].services.jwt.getToken(ctx.context);
+              userid = decrypted.id;
+            } catch (e) {
+              console.log(e)
               errNum = "201";
               errDesc = 'Invalid token: Token did not contain required fields';
-            } else {
-              //Authentiation OK
-              try {
 
-                //CHECK if the item  belongs to crt user
-                const chkResult = await strapi.services.spot.find({uid: uid, user: userid, deleted: false});
-                console.log(chkResult);
-                if (chkResult.length > 0) {
-                  const id = chkResult[0].id;
-                  const updateData = {"deleted": true};
-                  payload = await strapi.services.spot.update({id}, updateData);
-                  Success = true;
-                  result = {
-                    "success": true,
-                    "payload": payload
-                  };
+            }
+          }
+          if (userid === undefined) {
+            errNum = "201";
+            errDesc = 'User undefined';
+          } else {
+            //Authentiation OK
+            try {
 
-                } else {
-                  errNum = "301";
-                  errDesc = 'Invalid spot id';
-                }
 
-              } catch (error) {
-                console.log(error);
+              //CHECK if the item  belongs to crt user
+              const chkResult = await strapi.services.spot.find({uid: uid, user: userid, deleted: false});
+              if (chkResult.length > 0) {
+                const id = chkResult[0].id;
+                payload = await strapi.services.spot.update({id}, options.data);
+                Success = true;
                 result = {
-                  "success": false,
-                  "payload": {},
-                  "error": {"code": error.errno, "message": "An error occurred! " + error.code}
+                  "success": Success,
+                  "payload": payload
                 };
 
-
+              } else {
+                errNum = "301";
+                errDesc = 'Invalid spot id';
               }
-            }
-            if (Success) {
-              //console.log(result);
-              return result;
 
-            } else {
 
+
+            } catch (error) {
+              console.log(error);
               result = {
                 "success": false,
                 "payload": {},
-                "error": {"code": errNum, "message": errDesc}
+                "error": {"code": error.errno, "message": "An error occurred! " + error.code}
               };
-              return result;
+
+
             }
           }
-        }
+          if (Success) {
+            //console.log(result);
+            return result;
 
+          } else {
+
+            result = {
+              "success": false,
+              "payload": {},
+              "error": {"code": errNum, "message": errDesc}
+            };
+            return result;
+          }
+        }
+      },
+
+      deleteUserSpot: {
+        description: 'Delete user spot',
+        resolverOf: 'application::spot.spot.find',
+        resolver: async (obj, options, ctx) => {
+          const uid = options.uid;
+          let newRecord;
+          let userid;
+          let result;
+          let payload;
+          let errNum;
+          let errDesc;
+          let Success = false;
+
+
+          //GET USER FROM HEADER TOKEN
+          if (ctx.context.request && ctx.context.request.header && ctx.context.request.header.authorization) {
+            try {
+              const decrypted = await strapi.plugins['users-permissions'].services.jwt.getToken(ctx.context);
+              userid = decrypted.id;
+            } catch (e) {
+              errNum = "201";
+              errDesc = 'Invalid token: Token did not contain required fields';
+
+            }
+          }
+          if (userid === undefined) {
+            errNum = "201";
+            errDesc = 'Invalid token: Token did not contain required fields';
+          } else {
+            //Authentiation OK
+            try {
+
+              //CHECK if the item  belongs to crt user
+              const chkResult = await strapi.services.spot.find({uid: uid, user: userid, deleted: false});
+              console.log(chkResult);
+              if (chkResult.length > 0) {
+                const id = chkResult[0].id;
+                const updateData = {"deleted": true};
+                payload = await strapi.services.spot.update({id}, updateData);
+                Success = true;
+                result = {
+                  "success": true,
+                  "payload": payload
+                };
+
+              } else {
+                errNum = "301";
+                errDesc = 'Invalid spot id';
+              }
+
+            } catch (error) {
+              console.log(error);
+              result = {
+                "success": false,
+                "payload": {},
+                "error": {"code": error.errno, "message": "An error occurred! " + error.code}
+              };
+
+
+            }
+          }
+          if (Success) {
+            //console.log(result);
+            return result;
+
+          } else {
+
+            result = {
+              "success": false,
+              "payload": {},
+              "error": {"code": errNum, "message": errDesc}
+            };
+            return result;
+          }
+        }
       }
+
     }
+  }
 };
 
