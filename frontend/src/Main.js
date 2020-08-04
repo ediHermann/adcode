8d7c6d7c5c34533a4d4fc31d787d409700a9596c
alina <alina.pindic@bitbrother.ro>
2020-08-04 13:48:00 +0300

    broadcastTable

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -7,7 +7,6 @@ import {
 import SpotList from "./pages/spot/spot_list"
 import Home from "./pages/home"
 import Login from "./pages/login"
-import Settings from "./pages/settings"
 import Profile from "./pages/profile/profile"
 import EditAvatar from "./pages/profile/edit_avatar"
 import RegisterTalent from "./pages/registerTalent"
@@ -15,11 +14,12 @@ import RegisterStudio from "./pages/registerStudio"
 import RegisterBroadcaster from "./pages/registerEmitator"
 import RegisterArbiter from "./pages/registerArbiter"
 import Layout from "./components/layout"
-import RegisterForm from "./components/register/register-form"
-import Other from "./pages/other"
+
 import LogOut from "./pages/logout"
 import CreateSpot from "./pages/spot/create_spot"
 import Other2 from "./pages/other2"
+import MySpotsNew from "./pages/mySpotsNew";
+import BroadcastTable from "./pages/broadcastTable";
 
 
 
@@ -34,13 +34,12 @@ class Main extends Component {
                         <div className="content">
                             <Route path="/home" render={() => <Home/>}/>
                             <Route path="/spot_list" render={() => <SpotList/>}/>
-                            <Route path="/settings" render={() => <Settings/>}/>
                             <Route path="/profile" render={() => <Profile/>}/>
-                            <Route path="/edit_avatar"  render={() => <EditAvatar/>}/>
-                            <Route path="/other1" render={() => <Other/>}/>
                             <Route path="/other2" render={() => <Other2/>}/>
                             <Route path="/create_spot" render={() => <CreateSpot/>}/>
                             <Route path="/logout"  render={() => <LogOut/>}/>
+                            <Route path="/my_spots" render={() => <MySpotsNew/>}/>
+                            <Route path="/other1" render={() => <BroadcastTable/>}/>
                         </div>
                     </Layout>
                 </HashRouter>
@@ -62,3 +61,4 @@ class Main extends Component {
 }
 
 export default Main;
+
